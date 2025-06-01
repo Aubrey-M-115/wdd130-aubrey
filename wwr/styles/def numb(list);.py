215e9c25152b1322c63f82cf13e0e578da6a7b3d
Aubrey-M-115 <slitherraven19@gmail.com>
2025-05-31 22:57:44 -0600

    week06

---
+++ wwr/styles/def numb(list);.py
@@ -0,0 +1,47 @@
+
+
+
+#list: food (breakfast, lunch, dinner) or eating out, 
+# water (calculate water),
+#hotel (different hotel for each night), 
+# mile plan per day and gas consumption,
+#map,
+#cash on the side for safety
+# supplies i.e.: blanket, pillow, shoes, shirt, pants, undergarments, socks, hairbrush, toothbrush, toothpaste, water bottle, cleansing stuff, jacket, swimsuit, etc.
+# cost of gas, food, supplies not purchased, snacks, cash to be taken out
+
+
+
+
+# It would start on figuring out lists of what each thing on the earlier written list
+#In that list, take in the cost of everything and calculate the total
+#Make a seperate list for supplies and return the list for people to see.
+#For mileage, determine the miles and divide that by miles per gallon. Then multiply that by the cost per gallon based on where you plan to stop at.
+# Once that is all done, add the cost of everything. show that cost and print the supply list
+# Make a function that shows everything on the list, and when it is done, you write it down on the bar, and it gets deleted once packed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def food():
+    food_cost = "What is the cost of all this food?"
+    return food_cost
+def hotel():
+    hotel_cost1 = "What is the cost of the first hotel?"
+    hotel_cost2 = "what is teh cost of the hotel on the second night?"
+    return hotel_cost1, hotel_cost2
+def mile_plan
