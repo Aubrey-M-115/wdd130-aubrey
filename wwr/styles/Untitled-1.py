6fc6628cf0f2de56beb9edc9fb8b3c7126fc8546
Aubrey-M-115 <slitherraven19@gmail.com>
2025-06-14 22:54:08 -0600

    homework

--- wwr/styles/Untitled-1.py
+++ wwr/styles/Untitled-1.py
@@ -1,8 +1,12 @@
+def numbers(number):
+    if isinstance (number, float):
+        answer = print(f"{number} is a decimal point and is not even")
+        return answer
+    if number%2 == 0:
+        answer = f"{number} is even"
+    else:
+        answer = f"{number} is odd"
+    print(answer)
+    return answer
 
-def number_list(for_avg):
-    if not for_avg:
-        return("There are no numbers in your list")
-    money = sum(for_avg)
-    average = money/len(for_avg)
-    return round(average, 2)
-print(number_list([5,9,7,1]))
+numbers(6.5)
