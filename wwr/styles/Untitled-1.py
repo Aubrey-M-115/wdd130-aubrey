aae5e55a6a232a6304ea688ee91f142716543b8b
Aubrey-M-115 <slitherraven19@gmail.com>
2025-06-18 13:16:28 -0600

    please work

--- wwr/styles/Untitled-1.py
+++ wwr/styles/Untitled-1.py
@@ -1,12 +1,17 @@
-def numbers(number):
-    if isinstance (number, float):
-        answer = print(f"{number} is a decimal point and is not even")
-        return answer
-    if number%2 == 0:
-        answer = f"{number} is even"
-    else:
-        answer = f"{number} is odd"
-    print(answer)
-    return answer
+# def numbers(number):
+#     if isinstance (number, float):
+#         answer = print(f"{number} is a decimal point and is not even")
+#         return answer
+#     if number%2 == 0:
+#         answer = f"{number} is even"
+#     else:
+#         answer = f"{number} is odd"
+#     print(answer)
+#     return answer
 
-numbers(6.5)
+# numbers(6.5)
+
+mult_table = []
+def number():
+    
+    for 
