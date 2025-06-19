c0811bb401b0df21932d5da6b2185c80b9f91cd7
Aubrey-M-115 <slitherraven19@gmail.com>
2025-06-18 19:54:28 -0600

    w07 finishing

--- wwr/styles/Untitled-1.py
+++ wwr/styles/Untitled-1.py
@@ -11,7 +11,30 @@
 
 # numbers(6.5)
 
-mult_table = []
-def number():
-    
-    for 
+# mult_table = []
+# def number():
+
+#     for 
+
+
+# import math
+
+# def square(number):
+#     square_root = number ** 0.5
+#     print(square_root)
+#     return square_root
+
+# square(6)
+
+# def double()
+
+
+def parameter(number):
+    mult_table = []
+    for i in range(1,10):
+        answer = number * i
+        mult_table.append(answer)
+    print (mult_table)
+    return mult_table
+
+parameter(4)
