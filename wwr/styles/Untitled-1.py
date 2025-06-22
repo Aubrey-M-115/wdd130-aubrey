977d52f9f4b1bbc9ccad1af3e05f5fb4f9064cb8
Aubrey-M-115 <slitherraven19@gmail.com>
2025-06-21 18:28:42 -0600

    wk08.p2

--- wwr/styles/Untitled-1.py
+++ wwr/styles/Untitled-1.py
@@ -29,12 +29,75 @@
 # def double()
 
 
-def parameter(number):
-    mult_table = []
-    for i in range(1,10):
-        answer = number * i
-        mult_table.append(answer)
-    print (mult_table)
-    return mult_table
+# def parameter(number):
+#     mult_table = []
+#     for i in range(1,10):
+#         answer = number * i
+#         mult_table.append(answer)
+#     print (mult_table)
+#     return mult_table
 
-parameter(4)
+# parameter(4)
+
+# def giving_away(message = "I love you"):
+#     print(f"{message}, son")
+
+# def giving_away(default_message = "I love you", message = None):
+#     if not message:
+#         print(f"{default_message}, son")
+#     if message:
+#         print(f"{message}, son")
+
+# giving_away("I hate you")
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# lambda x: x % 2 == 0
+# filter(lambda x: x % 2 == 0, numbers)
+# print(list(filter(lambda x: x % 2 == 0, numbers)))
+
+# def rect(width = 1, height = 1):
+#     if not width and height:
+#         answer = width * height
+#         print(answer)
+#         return answer
+#     if width and height:
+#         answer = width * height
+#         print(answer)
+#         return answer
+
+# rect(3, 5)
+
+# times_three = lambda x: x*3
+
+# two_numb = lambda x,y: x+y
+
+# print(two_numb(3.5,2.5))
+
+# numbers = [1, 2, 3, 4]
+
+# print(list(map(times_three, numbers)))
+
+
+# numbers = [2,5,3,9,6,8,7,9]
+# sorting = lambda x: x
+# print(sorted(numbers, key=sorting))
+
+# numbers = [2, 5, 3, 9, 6, 8, 7, 9]
+# print(sorted(numbers, reverse=True))
+
+
+# dictionary = [7, 9, 2, 4, 7, 6, 9, 0, 1]
+# filter(lambda x: x = "l", dictionary)
+# print(list(filter(lambda x: x = "l", dictionary)))
+
+# numbers = [1, 7, 5, 9, 4, 7, 5, 0, -1]
+# abso_lute = lambda x: abs(x)
+# print(sorted(numbers, key=abso_lute))
+
+def debug_task4(s):
+    rev = ""
+    for char in s:
+         rev = char + rev
+    return rev
+
+print(debug_task4("regty"))
